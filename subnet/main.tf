a8e14b1e74e55671d6647e18e0ac69ab8685b706
Zebo Alieva <zeboalieva@air-zebo.mynetworksettings.com>
2023-10-09 14:01:20 -0400

    change content for oct4

--- subnet/main.tf
+++ subnet/main.tf
@@ -6,4 +6,36 @@ terraform {
       name = "infra-subnet"
     }
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-0bb08df1093ea4c4f"
+}
+
+variable "subnets" {
+  type = map(object({
+    cidr_block = string
+  }))
+  default = {}
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
 }
--- vpc/main.tf
+++ vpc/main.tf
@@ -6,4 +6,23 @@ terraform {
       name = "infra-vpc"
     }
   }
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
